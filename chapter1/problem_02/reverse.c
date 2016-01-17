46b94e629db6e92f7da25bf4db6f00299a2dcabe
David Alberto Montaño Fetecua <damontic@gmail.com>
2016-01-17 17:26:23 -0500

    problem 2 solved

---
+++ chapter1/problem_02/reverse.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char* str){
+  int myStringLength = strlen(str);
+  char* newString = malloc(sizeof(char) * myStringLength);
+  char temp;
+  for(int i = 0; i < myStringLength/2; i++){
+    temp = str[i];
+    str[i] = str[myStringLength -i -1];
+    str[myStringLength -i -1] = temp;
+  }
+}
+
+int main(int argc, char** args){
+  if(argc == 1){
+    fprintf(stderr, "Usage: ./reverse STRING\n");
+    return 1;
+  }
+  
+  reverse(args[1]);
+  printf("%s\n", args[1]);
+  return 0;
+}
